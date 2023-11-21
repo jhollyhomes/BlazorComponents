cc1fb0b6b728f07be168986fcae7acdc950516b7
James Hollyhomes <jamie.hollyhomes@hotmail.co.uk>
2023-11-21 23:08:58 +0000

    initial checkin, working mutiselect.  need to get delete on item working.

---
+++ src/Mms.Components/Pages/Components/MultiSelect.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace Mms.Components.Pages.Components;
+
+public partial class MultiSelect
+{
+    public List<DropDownItem> items { get; set; } = new();
+
+    protected override void OnInitialized()
+    {
+        GetItems();
+    }
+
+    void GetItems()
+    {
+        items = new()
+        {
+            new DropDownItem("NS", "Not started"),
+            new DropDownItem("IP", "In progress"),
+            new DropDownItem("CO", "Completed"),
+            new DropDownItem("CA", "Cancelled"),
+            new DropDownItem("OH", "On hold"),
+            new DropDownItem("RA", "Reassigned")
+        };
+    }
+}
+
+public class DropDownItem(string key, string value)
+{
+    public string Key { get; set; } = key;
+    public string Value { get; set; } = value;
+    public bool IsChecked { get; set; } = true;
+}
