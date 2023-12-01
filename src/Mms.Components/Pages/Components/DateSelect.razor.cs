3a9d139980228e9326ee98180b2fe24ca64a235a
James Hollyhomes <jamie.hollyhomes@hotmail.co.uk>
2023-12-01 01:48:05 +0000

    added display of a month of days

--- src/Mms.Components/Pages/Components/DateSelect.razor.cs
+++ src/Mms.Components/Pages/Components/DateSelect.razor.cs
@@ -5,11 +5,31 @@ namespace Mms.Components.Pages.Components;
 public partial class DateSelect
 {
     [Parameter]
-    public DateOnly InitialDate { get; set; }
+    public DateOnly InitialDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
     private bool _visible = false;
 
     void ShowPopup() => _visible = true;
     void ClosePopup() => _visible = false;
     void TogglePopup() => _visible = !_visible;
+
+    void DateChanged(int day)
+    {
+
+    }
+
+    private int FirstDayOfMonth => new DateOnly(InitialDate.Year, InitialDate.Month, 1).Day;
+    private int DaysInMonth => DateTime.DaysInMonth(InitialDate.Year, InitialDate.Month);
+    private List<string> _daysOfWeek = new()
+    { 
+        "Su", 
+        "Mo",
+        "Tu",
+        "We",
+        "Th",
+        "Fr",
+        "Sa"
+    };
 }
+
+
