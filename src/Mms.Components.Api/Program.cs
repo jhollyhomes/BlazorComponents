62aca3dd2a8fcafb36a24d3b0df023ae294cf1ef
James Hollyhomes <jamie.hollyhomes@hotmail.co.uk>
2023-12-23 10:21:52 +0000

    moved endpoints to make testable, added tests for api in new test project

--- src/Mms.Components.Api/Program.cs
+++ src/Mms.Components.Api/Program.cs
@@ -7,12 +7,6 @@ builder.Services.AddSingleton<IFakeCompanyService, FakeCompanyService>();
 
 var app = builder.Build();
 
-var mapGroup = app.MapGroup("api/company");
-mapGroup.MapGet("/", GetCompanys);
+
 
 app.Run();
-
-static List<Company> GetCompanys(IFakeCompanyService companyService)
-{
-    return companyService.Companies;
-}
